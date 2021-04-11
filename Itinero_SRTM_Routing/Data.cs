02f08c9156f4608ca11da60ecad0f6e93842fd66
Stefan Nufer <60579655+SNUF2020@users.noreply.github.com>
2021-04-11 19:08:28 +0200

    Add files via upload

---
+++ Itinero_SRTM_Routing/Data.cs
@@ -0,0 +1,24 @@
+﻿namespace RPS
+{
+    class Data
+    {
+        public double Lat { get; set; }
+        public double Lon { get; set; }
+        public double Alt { get; set; }
+        public int Time { get; set; }
+        public double Distance { get; set; }
+
+        //
+        // ----------------------------------------------------------------------------------------
+        // 
+        // Constructors
+        public Data(double Breite, double Länge, double Höhe, int Zeit, double Strecke)
+        {
+            Lat = Breite;
+            Lon = Länge;
+            Alt = Höhe;
+            Time = Zeit;
+            Distance = Strecke;
+        } // first overload - for initial construction of new location
+    }
+}
